e4675e69392e3c663deaaaac3c668d2b181e3df7
leon0113 <tahjib.leon399@gmail.com>
2023-02-10 03:32:58 +0600

    link for every country

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Countries = (props) => {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch("https://restcountries.com/v3.1/all")
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
     return (
         <div>
             <h2>Welcome to rest Countries</h2>
+            <ul>
+                {
+                    countries.map(country => <li> <Link to={`/country/${country.name.common}`}>{country.name.common}</Link> </li>)
+                }
+            </ul>
         </div>
 
     );
